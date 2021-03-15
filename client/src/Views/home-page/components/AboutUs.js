39a3c465b7557f867860d646725c05664ad4c2c1
borabii <benouirane.rabii@gmail.com>
2021-03-15 11:57:08 +0100

    improvement

---
+++ client/src/Views/home-page/components/AboutUs.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./AboutUs.css";
+function AboutUs() {
+  return (
+    <div className="aboutUS">
+      <h1>about us</h1>
+    </div>
+  );
+}
+
+export default AboutUs;
---
+++ client/src/Views/home-page/components/TopUserPerSport.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./TopThreeUser.css";
+function TopUserPerSport() {
+  return (
+    <div className="topUserPerSport">
+      <h1>top three user</h1>
+    </div>
+  );
+}
+
+export default TopUserPerSport;
