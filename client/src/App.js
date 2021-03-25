e013ddf3798da2dc273f48143fd1f72e99816a32
borabii <wissal.msdi@gmail.com>
2021-03-25 14:23:54 +0100

    improvement

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,15 @@ import AdminHomePage from "./Views/admin-pages/AdminHomePage";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./Views/home-page/Home";
 import DetailaAnoncePopUp from "./Views/admin-pages/component/DetailaAnoncePopUp";
+import DetailaEventPopUp from "./Views/admin-pages/component/DetailEventPopUp";
 function App() {
   return (
     <div className="app">
       {/* <Home /> */}
       {/* <AdminHomePage /> */}
+      <DetailaEventPopUp/>
     
-      <DetailaAnoncePopUp />
+     
     </div>
   );
 }
