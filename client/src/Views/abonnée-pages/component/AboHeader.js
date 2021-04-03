b52de428e814dda11d2aa94fd6c9c11a4323f28f
borabii <benouirane.rabii@gmail.com>
2021-04-04 00:19:28 +0100

    improvement

--- client/src/Views/abonnée-pages/component/AboHeader.js
+++ client/src/Views/abonnée-pages/component/AboHeader.js
@@ -1,66 +1,108 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import NotificationsIcon from "@material-ui/icons/Notifications";
 import SearchIcon from "@material-ui/icons/Search";
 import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
-
+import ReactSearchBox from "react-search-box";
 function AboHeader() {
+  const [showInfo, setshowInfo] = useState(false);
+  const data = [
+    {
+      key: "john",
+      value: "John Doe",
+    },
+    {
+      key: "jane",
+      value: "Jane Doe",
+    },
+    {
+      key: "mary",
+      value: "Mary Phillips",
+    },
+    {
+      key: "robert",
+      value: "Robert",
+    },
+    {
+      key: "karius",
+      value: "Karius",
+    },
+  ];
   return (
     <Navbar collapseOnSelect expand="lg" className="abonné-Header" fixed="top">
-      <Navbar.Brand className="">
-        <h2>Bienvenu, Jones</h2>
-      </Navbar.Brand>
-      <Navbar.Toggle
-        aria-controls="navbar-collpase"
-        id="navbar-collapse-small-device"
-      />
+      <div>
+        <Navbar.Brand className="">
+          <h2>Bienvenu, Jones</h2>
+        </Navbar.Brand>
+      </div>
 
-      <Nav className=" justify-content-center align-middle  ml-auto">
-        <div className="aboHeader-searsh">
-          <div className="searshSelect">
-            <select id="dropdown-searshOption">
-              <option>Annonce</option>
-              <option>Activity</option>
-              <option>Evenement</option>
-              <option>Abonné</option>
-            </select>
-          </div>
+      <div>
+        <Nav>
+          <div className="aboHeader-searsh">
+            <div className="searshSelect">
+              <select id="dropdown-searshOption">
+                <option>Annonce</option>
+                <option>Activity</option>
+                <option>Evenement</option>
+                <option>Abonné</option>
+              </select>
+            </div>
 
-          <input type="text" placeholder=" Recherche" />
-          <div className="aboHeader-searshIcon">
-            <SearchIcon id="searshIcon" />
-          </div>
-        </div>
-      </Nav>
-      <Navbar.Toggle
-        aria-controls="navbar-collpase"
-        id="navbar-collapse-lg-device"
-      />
-      <Navbar.Collapse className="justify-content-center" id="navbar-collpase">
-        <Nav className="ml-auto">
-          <Nav.Link>
-            <div className="header-chatIcon">
-              <ChatBubbleOutlineIcon id="chatIcon" />
-            </div>
-            <h4 id="btnChat-small-device">Boite Message</h4>
-          </Nav.Link>
-          <Nav.Link>
-            <div className="header-notifIcon">
-              <NotificationsIcon id="notifIcon" />
-            </div>
-            <h4 id="btnNotif-small-device">Notification</h4>
-          </Nav.Link>
-          <Nav.Link>
-            <img
-              src="https://learnenglishteens.britishcouncil.org/sites/teens/files/styles/article/public/istock_000016994756small.jpg?itok=yczzK-18"
-              alt=""
-              className="userPicture"
+            <input
+              type="text"
+              placeholder=" Recherche"
+              onClick={() => setshowInfo(true)}
+              onBlur={() => setshowInfo(false)}
             />
-            <h4 id="btnProfil-small-device">Profile</h4>
-          </Nav.Link>
+            <div className="aboHeader-searshIcon">
+              <SearchIcon id="searshIcon" />
+            </div>
+            <div
+              className="searsh-resault"
+              style={{ display: showInfo ? "block" : "none" }}
+            >
+              ddddddddddddddddddddddddddddddddddd
+            </div>
+          </div>
         </Nav>
-      </Navbar.Collapse>
+      </div>
+      <div id="navbar-collapse-small-device">
+        <Navbar.Toggle aria-controls="navbar-collpase" />
+      </div>
+      <div>
+        <Navbar.Toggle
+          aria-controls="navbar-collpase"
+          id="navbar-collapse-lg-device"
+        />
+        <Navbar.Collapse
+          className="justify-content-center"
+          id="navbar-collpase"
+        >
+          <Nav>
+            <Nav.Link>
+              <div className="header-chatIcon">
+                <ChatBubbleOutlineIcon id="chatIcon" />
+              </div>
+              <h4 id="btnChat-small-device">Boite Message</h4>
+            </Nav.Link>
+            <Nav.Link>
+              <div className="header-notifIcon">
+                <NotificationsIcon id="notifIcon" />
+              </div>
+              <h4 id="btnNotif-small-device">Notification</h4>
+            </Nav.Link>
+            <Nav.Link>
+              <img
+                src="https://learnenglishteens.britishcouncil.org/sites/teens/files/styles/article/public/istock_000016994756small.jpg?itok=yczzK-18"
+                alt=""
+                className="userPicture"
+              />
+              <h4 id="btnProfil-small-device">Profile</h4>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </div>
     </Navbar>
   );
 }
