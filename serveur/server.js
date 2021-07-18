e90f611e2220e91b70bc54fcf2cd638e7f93f601
borabii <benouirane.rabii@gmail.com>
2021-07-18 11:38:14 +0100

    deploy the app

--- serveur/server.js
+++ serveur/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const connectDB = require("./config/db");
 var cors = require("cors");
 var app = express();
-
+const path = require("path");
 //// Init Middleware
 app.use(cors());
 app.use(express.json({ extended: false }));
@@ -18,9 +18,16 @@ app.use("/api/Categorie", require("./routes/Catégorie"));
 app.use("/api/Contact", require("./routes/Contact"));
 app.use("/api/Reclamation", require("./routes/Reclamation"));
 app.use("/api/Notification", require("./routes/Notification"));
-
 app.use("/api/Conversations", require("./routes/Conversations"));
 app.use("/api/Messages", require("./routes/Messages"));
+
+//
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 //define servert port
 const PORT = process.env.PORT || 8000;
 
